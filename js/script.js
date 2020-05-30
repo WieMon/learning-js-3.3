637bcd9c4fcded11f9d9d1e5f3478c29dfe6d7c7
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-30 13:02:39 +0200

    Add creating a list and adding new items on click

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let size = 10;
+let orderElement = 1;
+
+
+const init = () => {
+  const btn = document.createElement('button');
+  btn.textContent = 'Add 10 elements';
+  document.body.appendChild(btn);
+
+  const ul = document.createElement('ul');
+  ul.style.listStyle = 'none';
+  document.body.appendChild(ul);
+    
+  btn.addEventListener('click', createLiElements)
+}
+
+const createLiElements = () => {
+  console.log('jest ok')
+  
+  for(let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Element number ${orderElement++}`;
+    li.style.fontSize = `${size++}px`
+    document.querySelector('ul').appendChild(li);
+  }
+}
+
+init()
